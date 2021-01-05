9d22408a3d269084dcc7ad9013e10c77cffa8ae1
Will Xiao <xiaoshuoit@163.com>
2021-01-05 16:19:03 +0800

    查找第 K 大数

--- KthNum.js
+++ KthNum.js
@@ -1,4 +1,24 @@
 /**
+ * 查找第 K 大元素（利用快排思想实现）
+ */
+function KthNum(arr, k) {
+  return _KthNum(arr, k, 0, arr.length - 1);
+}
+
+function _KthNum(arr, k, p, r) {
+  if (k < 1 || k > arr.length) return false;
+  if (p >= r) return arr[p];
+  const q = partition(arr, p, r);
+  if (q === k - 1) {
+    return arr[q];
+  } else if (q > k - 1) {
+    return _KthNum(arr, k, p, q - 1);
+  } else {
+    return _KthNum(arr, k, q + 1, r);
+  }
+}
+
+/**
  * 分区算法
  */
 function partition(arr, p, r) {
@@ -7,36 +27,25 @@ function partition(arr, p, r) {
   for (let j = p; j <= r - 1; j++) {
     if (arr[j] > pivot) {
       // 当有元素比 pivot 大的时候，跟下标为 i 的元素交换位置，i ++
-      const temp = arr[i];
-      arr[i] = arr[j];
-      arr[j] = temp;
+      if (j !== i) {
+        const temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
       i++;
     }
   }
 
   // 最后，交换 pivot 元素
-  arr[r] = arr[i];
-  arr[i] = pivot;
+  if (i !== r) {
+    arr[r] = arr[i];
+    arr[i] = pivot;
+  }
 
   return i;
 }
 
-/**
- * 查找第 K 大元素（利用快排思想实现） 
- */
-function KthNum(arr, p, r, k) {
-  if (k > arr.length) return false;
-  if (p >= r) return arr[p];
-  const q = partition(arr, p, r);
-  if (q + 1 === k) {
-    return arr[q];
-  } else if (q + 1 > k) {
-    return KthNum(arr, p, q - 1, k);
-  } else {
-    return KthNum(arr, q + 1, r, k);
-  }
-}
-
 const arr = [8, 10, 2, 3, 6, 1, 5];
-const res = KthNum(arr, 0, 6, 3);
-console.log(res);
+console.log(KthNum(arr, 1));
+console.log(KthNum(arr, 2));
+console.log(KthNum(arr, 3));
